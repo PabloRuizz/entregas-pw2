54367b28cc423697b9b3a0e295d4847cbc282930
PabloRuizz <pabloruiz6@gmail.com>
2024-05-13 19:35:53 +0200

    Ejemplo computed Vue

---
+++ Entrega 2/ejemplo-computed-vue/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>Contador: {{ contador }}</p>
+    <button @click="incrementar">Incrementar</button>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+
+  // Definimos el contador como una referencia reactiva con valor inicial de 0
+  const contador = ref(0);
+
+  // Función para incrementar el contador
+  function incrementar() {
+    contador.value++;
+  }
+</script>
