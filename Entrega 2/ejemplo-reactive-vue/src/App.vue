1a1c6c2913d9bdf86e3c6ef03721266e0f224ea0
PabloRuizz <pabloruiz6@gmail.com>
2024-05-13 19:43:21 +0200

    Ejemplo reactive Vue

---
+++ Entrega 2/ejemplo-reactive-vue/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <p>Contador: {{ contador }}</p>
+      <button @click="incrementar">Incrementar</button>
+    </div>
+  </template>
+  
+  <script setup>
+    import { reactive } from 'vue';
+  
+    // Definimos el estado del componente como un objeto reactiva
+    const state = reactive({
+      contador: 0
+    });
+  
+    // Función para incrementar el contador
+    function incrementar() {
+      state.contador++;
+    }
+  </script>
+  
