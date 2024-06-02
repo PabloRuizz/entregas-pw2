51158e48dcafaaeeb45b766b355b718acbe46fd6
PabloRuizz <pabloruiz6@gmail.com>
2024-06-02 13:16:52 +0200

    Ejemplo watch Vue

---
+++ Entrega 2/ejemplo-watch-vue/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h2>Detalles del Producto</h2>
+    <p>Nombre del Producto: {{ producto.nombre }}</p>
+    <p>Precio Unitario: ${{ producto.precio }}</p>
+    <p>Cantidad: <input type="number" v-model="cantidad"></p>
+    <p>Precio Total: ${{ precioTotal }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      producto: {
+        nombre: 'Camisa',  
+        precio: 25.99      
+      },
+      cantidad: 1          // Cantidad inicial del producto
+    };
+  },
+  computed: {
+    // Propiedad computada para mostrar el precio total
+    precioTotal() {
+      return this.producto.precio * this.cantidad;
+    }
+  },
+  watch: {
+    // Vigila cambios en la cantidad
+    cantidad(newCantidad) {
+      // Actualiza el precio total cuando cambia la cantidad
+      this.actualizarPrecioTotal(newCantidad);
+    }
+  },
+  methods: {
+    // Método para actualizar el precio total
+    actualizarPrecioTotal(cantidad) {
+      this.precioTotal = this.producto.precio * cantidad;
+    }
+  }
+};
+</script>
